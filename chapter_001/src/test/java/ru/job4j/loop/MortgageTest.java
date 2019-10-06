714ccc81945ce61580ea0a504c9a098b4de5ae2e
sevsilver1982 <suvorovev@mail.ru>
2019-10-06 20:08:35 +0500

    5.7. Ипотека.[#176879]

---
+++ chapter_001/src/test/java/ru/job4j/loop/MortgageTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MortgageTest {
+
+    @Test
+    public void when1Year() {
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(1000, 1200, 1);
+        assertThat(year, is(1));
+    }
+
+    @Test
+    public void when2Year() {
+        Mortgage mortgage = new Mortgage();
+        int year = mortgage.year(100, 120, 50);
+        assertThat(year, is(2));
+    }
+
+}
