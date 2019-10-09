08ad7b46930a45ececd5f995cbd1e4b647873bf6
sevsilver1982 <suvorovev@mail.ru>
2019-10-09 12:45:08 +0500

    6.2. Перевернуть массив.[#176892]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+public class EndsWith {
+
+    /**
+     * Проверка, что массив word имеет последние элементы одинаковые с последовательностью post
+     * @param word Массив
+     * @param post последовательность
+     * @return Вернёт true, если массив заканчивается последовательностью post
+     */
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+
+        for (int index = 0; index < post.length; index++) {
+            if (word[word.length - post.length + index] != post[index]) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+}
