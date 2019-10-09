51753487b463bb572dbe452be27cd6f7487798d4
sevsilver1982 <suvorovev@mail.ru>
2019-10-09 11:55:28 +0500

    6.2. Перевернуть массив.[#176892] Доработка по замечаниям: Andrei Hincu: for (int index = 0; index != array.length; index++) { лучше сразу for (int index = 0; index != array.length / 2; index++)  {

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -13,14 +13,11 @@ public class Turn {
         int half = (array.length / 2);
 
         if (half % 2 == 0) half--;
-        for (int index = 0; index != array.length; index++) {
+        for (int index = 0; index <= half; index++) {
             i = array.length - 1 - index;
             tmp = array[index];
             array[index] = array[i];
             array[i] = tmp;
-            if (index == half) {
-                break;
-            }
         }
         return array;
     }
