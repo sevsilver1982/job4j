c1844e383ebc9cd1be3c3bcc5c21c3130545b9ae
sevsilver1982 <suvorovev@mail.ru>
2019-10-16 22:34:04 +0500

    1.1 Котики обормотики.[#177002]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+/**
+ * Кошка
+ */
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+/**
+ * Собака
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
