2874fe9e0572aa9ae109c05cea61dd4fdd038321
sevsilver1982 <suvorovev@mail.ru>
2019-10-17 13:50:22 +0500

    1.6. Петя и красная шапочка.[#177006]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+/**
+ * Колобок
+ */
+public class Ball {
+    public String name = "Колобок";
+
+    public void onEat(Object eater) {
+        System.out.println(
+            this.name + ": " + "Я колобок, колобок!\n" +
+            this.name + ": " + "По амбару метён,\n" +
+            this.name + ": " + "По сусекам скребён,\n" +
+            this.name + ": " + "На сметане мешан,\n" +
+            this.name + ": " + "В печку сажён,\n" +
+            this.name + ": " + "На окошке стужён,\n" +
+            this.name + ": " + "Я от дедушки ушёл,\n" +
+            this.name + ": " + "Я от бабушки ушёл:");
+
+        if (eater instanceof Hare) {
+            System.out.println(this.name + ": " + "И от тебя, зайца, не хитро уйти.\n");
+        } else {
+            if (eater instanceof Wolf) {
+                System.out.println(this.name + ": " + "И от тебя, волка, не хитро уйти.\n");
+            } else {
+                if (eater instanceof Fox) {
+                    System.out.println(this.name + ": " + "И от тебя, лисы, не хитро уйти.\n");
+                }
+            }
+        }
+    }
+
+}
---
+++ chapter_002/src/main/java/ru/job4j/oop/Wolf.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+/**
+ * Волк
+ */
+public class Wolf {
+    public String name = "Волк";
+
+    public void tryEat(Ball ball) {
+        System.out.println(this.name + ": " + "Колобок, колобок! Я тебя съем");
+        ball.onEat(this);
+    }
+
+    public void eat(Girl girl) {
+
+    }
+
+}
