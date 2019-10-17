19142edf01912aad5fbca1844b7f057f3cadb3ae
sevsilver1982 <suvorovev@mail.ru>
2019-10-17 12:35:05 +0500

    1.4. Переводчик Русский-Английский.[#177007]

---
+++ chapter_002/src/main/java/ru/job4j/oop/DummyDic.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+public class DummyDic {
+
+    public String engToRus(String eng) {
+        String answer = "Неизвестное слово: " + eng;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DummyDic etr = new DummyDic();
+        String answer = etr.engToRus("Hello");
+        System.out.println(answer);
+    }
+
+}
