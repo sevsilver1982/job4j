1aacbe0fcbb7930ef1257eef15c644abc822457c
sevsilver1982 <suvorovev@mail.ru>
2019-10-17 12:29:30 +0500

    1.4. Переводчик Русский-Английский.[#177007]

--- chapter_002/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -4,8 +4,16 @@ package ru.job4j.oop;
  * Кошка
  */
 public class Cat {
+
+    public String sound() {
+        String voice = "may-may";
+        return voice;
+    }
+
     public static void main(String[] args) {
         Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        String say = peppy.sound();
+        System.out.println("Peppy says " + say);
     }
+
 }
