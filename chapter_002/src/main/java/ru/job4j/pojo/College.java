1624243e3949003e5bc0f18f08fc95a60d1fa107
sevsilver1982 <suvorovev@mail.ru>
2019-10-19 19:18:28 +0500

    2. Модель данных.[#177020]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,15 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setSurname("Suvorov");
+        student.setName("Evgeniy");
+        student.setPatronymic("Vladimirovich");
+        student.setBeginDate(new Date());
+
+        System.out.println(student.getSurname() + " " + student.getName() + " " + student.getPatronymic() + " start at " + student.getBeginDate());
+    }
+}
