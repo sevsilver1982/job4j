e7cfe8be61c21553fc2da8bd61a12cf60a8475e4
sevsilver1982 <suvorovev@mail.ru>
2019-10-19 16:48:59 +0500

    4. Расстояние между точками в трехмерном пространстве.[#176999]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -3,30 +3,42 @@ package ru.job4j.condition;
 public class Point {
     private double x;
     private double y;
+    private double z;
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public double distance(Point point2) {
-        return Math.sqrt(Math.pow(point2.x - this.x, 2) + Math.pow(point2.y - this.y, 2));
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double distance(Point point) {
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
+    }
+
+    public double distance3d(Point point) {
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2) + Math.pow(point.z - this.z, 2));
     }
 
     public static double distance(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+
     public static void main(String[] args) {
-        double result;
+        Point point2d1 = new Point(0, 0);
+        Point point2d2 = new Point(2, 0);
+        System.out.println(point2d1.distance(point2d2));
 
-        Point point1 = new Point(0, 0);
-        Point point2 = new Point(2, 0);
-
-        result = distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) = " + result);
-
-        result = point1.distance(point2);
-        System.out.println("result (0, 0) to (2, 0) = " + result);
+        Point point3d1 = new Point(0, 0, -2);
+        Point point3d2 = new Point(2, 0, 2);
+        System.out.println(point3d1.distance3d(point3d2));
     }
 }
