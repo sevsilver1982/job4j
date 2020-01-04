38b9ef80c62c0321ce78e40848c5da2d9817e692
sevsilver1982 <suvorovev@mail.ru>
2020-01-04 06:31:36 +0500

    0.1 Ссылки на методы.[#176831]

--- chapter_004/src/main/java/Calculator.java
+++ chapter_004/src/main/java/Calculator.java
@@ -4,13 +4,13 @@ import java.util.function.Consumer;
 public class Calculator {
 
     public void multiple(int start, int finish, int value, BiFunction<Integer, Integer, Double> function, Consumer<Double> consumer) {
-        for (int index = start; index != finish; index++) {
+        for (int index = start; index < finish; index++) {
             consumer.accept(function.apply(value, index));
         }
     }
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
+        /*Calculator calc = new Calculator();
         calc.multiple(
                 0, 10, 2,
                 (value, index) -> {
@@ -19,6 +19,13 @@ public class Calculator {
                     return result;
                 },
                 result -> System.out.println(result)
+        );*/
+
+        Calculator calc = new Calculator();
+        calc.multiple(
+                0, 10, 1,
+                MathUtil::add,
+                System.out::println
         );
     }
 
---
+++ chapter_004/src/main/java/MathUtil.java
@@ -0,0 +1,11 @@
+public class MathUtil {
+
+    public static double add(int left, int second) {
+        return left + second;
+    }
+
+    public static double div(int left, int second) {
+        return left / second;
+    }
+
+}
