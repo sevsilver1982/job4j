c5666f5f18279492376a1a45c6f71e7410fe3693
sevsilver1982 <suvorovev@mail.ru>
2020-01-10 18:38:07 +0500

    #R

--- chapter_004/src/main/java/school/StudentsToMap.java
+++ chapter_004/src/main/java/school/StudentsToMap.java
@@ -2,23 +2,24 @@ package school;
 
 import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StudentsToMap {
 
     public static void main(String[] args) {
         Map<String, Student> students =
-                Arrays.asList(
+                Stream.of(
                         new Student("name1", "surname1"),
                         new Student("name2", "surname2"),
                         new Student("name3", "surname3"),
                         new Student("name4", "surname4")
-                ).stream().distinct().collect(
+                ).distinct().collect(
                         Collectors.toMap(
                                 Student::getSurname,
                                 student -> student
                         )
                 );
-        students.forEach((s, student) -> System.out.println(student));
+        students.forEach((surname, student) -> System.out.println(student));
     }
 
 }
