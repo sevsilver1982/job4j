b0b1e7d2b1d49f23466a3d699b9a43c1cb72d9fe
sevsilver1982 <suvorovev@mail.ru>
2020-01-10 22:38:40 +0500

    5. Преобразование матрицы чисел в список чисел.[#176840]

---
+++ chapter_004/src/main/java/MatrixToList.java
@@ -0,0 +1,21 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MatrixToList {
+    static int position = 1;
+
+    public static void main(String[] args) {
+
+        Integer[][] matrix = new Integer[][] {
+                {0, 0, 1, 0, 0},
+                {0, 2, 0, 3, 0},
+                {4, 0, 5, 0, 9}
+        };
+
+        Stream.of(matrix)
+                .flatMap(Stream::of)
+                .collect(Collectors.toList())
+                .forEach(x -> System.out.printf("row %s = %s\n", position++, x));
+    }
+
+}
