d9606545ee8d96e127ecb778a0e698698677c5fc
sevsilver1982 <suvorovev@mail.ru>
2020-01-21 11:52:43 +0500

    0.2 Зона видимости в лямбда и исключения[#176832]

---
+++ chapter_004/src/main/java/math/Holder.java
@@ -0,0 +1,52 @@
+package math;
+
+import java.util.List;
+
+public class Holder {
+
+    public static void badMethod() throws Exception {
+    }
+
+    public interface Wrapper<T> {
+        T get();
+        void set(T value);
+        boolean isEmpty();
+    }
+
+    public static class ExpHold<T> implements Wrapper<T> {
+        private T value;
+
+        @Override
+        public T get() {
+            return this.value;
+        }
+
+        @Override
+        public void set(T value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return this.value == null;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        List<String> names = List.of("Petr", "Nick", "Ban");
+        Wrapper<Exception> ex = new ExpHold<>();
+        names.forEach(
+                n -> {
+                    try {
+                        badMethod();
+                    } catch (Exception e) {
+                        ex.set(e);
+                    }
+                }
+        );
+        if (!ex.isEmpty()) {
+            throw ex.get();
+        }
+    }
+
+}
