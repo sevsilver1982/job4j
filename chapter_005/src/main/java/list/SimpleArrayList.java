aa51283393c020ab0c98f1336ea945bbe50dbdb1
sevsilver1982 <suvorovev@mail.ru>
2020-01-30 16:54:46 +0500

    5.3.0 Создать метод delete для односвязного списка[#222033]

---
+++ chapter_005/src/main/java/list/SimpleArrayList.java
@@ -0,0 +1,65 @@
+package list;
+
+public class SimpleArrayList<E> {
+
+    private int size;
+    private Node<E> first;
+
+    /**
+     * Метод вставляет в начало списка данные.
+     */
+    public void add(E data) {
+        Node<E> newLink = new Node<>(data);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Реализовать метод удаления первого элемента в списке.
+     */
+    public E delete(int index) {
+        Node<E> prv = null;
+        Node<E> next = this.first;
+        for (int i = 0; i < index; i++) {
+
+            next = next.next;
+            prv = next.next;
+        }
+        this.first = prv;
+        prv = null;
+        this.size--;
+        return null;
+    }
+
+    /**
+     * Метод получения элемента по индексу.
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+
+}
---
+++ chapter_005/src/test/java/generic/list/SimpleArrayListTest.java
@@ -0,0 +1,41 @@
+package generic.list;
+
+import list.SimpleArrayList;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SimpleArrayListTest {
+
+    private SimpleArrayList<Integer> list;
+
+    @Before
+    public void beforeTest() {
+        list = new SimpleArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetOneResultTwo() {
+        assertThat(list.get(1), is(2));
+    }
+
+    @Test
+    public void whenAddThreeElementsThenUseGetSizeResultThree() {
+        assertThat(list.getSize(), is(3));
+    }
+
+    @Test
+    public void delete() {
+        list.delete(1);
+        assertThat(list.getSize(), is(3));
+    }
+
+}
