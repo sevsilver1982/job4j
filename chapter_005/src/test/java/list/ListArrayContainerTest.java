3d5a5121109e5817176553396045c714205561b2
sevsilver1982 <suvorovev@mail.ru>
2020-02-05 12:16:31 +0500

    #R

--- chapter_005/src/test/java/list/ListArrayContainerTest.java
+++ chapter_005/src/test/java/list/ListArrayContainerTest.java
@@ -9,7 +9,7 @@ import java.util.NoSuchElementException;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class ListContainerTest {
+public class ListArrayContainerTest {
 
     @Test
     public void add() {
--- chapter_005/src/test/java/list/SimpleNodeListTest.java
+++ chapter_005/src/test/java/list/SimpleNodeListTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class SimpleArrayListTest {
+public class SimpleNodeListTest {
 
     @Test
     public void whenAddThreeElementsThenUseGetOneResultTwo() {
