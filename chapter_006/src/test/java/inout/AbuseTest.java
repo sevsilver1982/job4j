ad5d7a5de57024b71efbf3412f733e7d31fa6880
sevsilver1982 <suvorovev@mail.ru>
2020-02-18 17:49:54 +0500

    2. Тестирование IO[#222070]

---
+++ chapter_006/src/test/java/inout/AbuseTest.java
@@ -0,0 +1,33 @@
+package inout;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class AbuseTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void drop() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("hello foolish dude ");
+        }
+        Abuse.drop(source.getAbsolutePath(), target.getAbsolutePath(), List.of("foolish"));
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is("hello dude "));
+    }
+
+}
