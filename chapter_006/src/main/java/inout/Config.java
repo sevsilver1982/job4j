4d14d297bdb05405fb4c8445233292ddc89125ba
sevsilver1982 <suvorovev@mail.ru>
2020-02-18 18:06:39 +0500

    1. Читаем файл конфигурации[#222071]

--- chapter_006/src/main/java/inout/Config.java
+++ chapter_006/src/main/java/inout/Config.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.StringJoiner;
 
 public class Config {
     private final String path;
@@ -41,21 +40,6 @@ public class Config {
 
     public String value(String key) {
         return values.get(key);
-    }
-
-    @Override
-    public String toString() {
-        StringJoiner out = new StringJoiner(System.lineSeparator());
-        try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            read.lines().forEach(out::add);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return out.toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Config(".\\chapter_006\\app.properties"));
     }
 
 }
--- chapter_006/src/test/java/inout/ConfigTest.java
+++ chapter_006/src/test/java/inout/ConfigTest.java
@@ -1,7 +1,13 @@
 package inout;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -9,9 +15,24 @@ import static org.hamcrest.Matchers.is;
 public class ConfigTest {
     private Config config;
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Before
-    public void load() {
-        config = new Config("app.properties");
+    public void load() throws IOException {
+        File source = folder.newFile("app.properties");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println(" = ##comment=");
+            out.println("");
+            out.println("нет знака равно");
+            out.println("hibernate.dialect=org.hibernate.dialect.PostgreSQLDialect");
+            out.println("hibernate.connection.url=jdbc:postgresql://127.0.0.1:5432/trackstudio");
+            out.println("hibernate.connection.driver_class=org.postgresql.Driver");
+            out.println("hibernate.connection.username=postgres");
+            out.println("hibernate.connection.password=password");
+            out.println("name= Petr Arsentev ##comment");
+        }
+        config = new Config(source.getAbsolutePath());
         config.load();
     }
 
