bbaa38db1616061f13f6d5ee4294105a1a2d5f46
sevsilver1982 <suvorovev@mail.ru>
2020-02-25 15:49:28 +0500

    4. Архивировать проект. [#222073]

--- chapter_006/src/main/java/inout/Zip.java
+++ chapter_006/src/main/java/inout/Zip.java
@@ -1,23 +1,23 @@
 package inout;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    //private
     private List<String> ext;
 
     public void addDirectory(ZipOutputStream zos, File dir) throws IOException {
         for (File file : dir.listFiles()) {
-            if(file.isDirectory()) {
+            if (file.isDirectory()) {
                 addDirectory(zos, file);
             } else {
                 if (!ext.contains(
                         file.getPath()
                                 .substring(file.getPath().lastIndexOf(".") + 1))) {
-
                     zos.putNextEntry(new ZipEntry(file.getPath()));
                     zos.write(new BufferedInputStream(new FileInputStream(file)).readAllBytes());
                     zos.closeEntry();
@@ -39,14 +39,45 @@ public class Zip {
     }
 
     public static void main(String[] args) {
+        String directory = "";
+        String exclude = "";
+        String output = "";
+
         for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-            /*if (arg.equals("-d")) {
-                directory
-            }*/
+            if (i + 1 > args.length) {
+                break;
+            }
+
+            switch (args[i]) {
+                case ("-d"):
+                    i++;
+                    directory = args[i];
+                    break;
+                case ("-e"):
+                    i++;
+                    exclude = args[i];
+                    break;
+                case ("-o"):
+                    i++;
+                    output = args[i];
+                    break;
+                default:
+                    break;
+            }
         }
-        //Zip zip = new Zip();
-        //zip.pack("./chapter_006/", "1.zip", List.of("exe", "txt"));
+
+        if (directory.isEmpty() || output.isEmpty()) {
+            return;
+        }
+
+        Zip zip = new Zip();
+        zip.pack(
+                directory,
+                output,
+                Arrays.stream(exclude.split(","))
+                        .map(String::trim)
+                        .collect(Collectors.toList())
+        );
     }
 
 }
