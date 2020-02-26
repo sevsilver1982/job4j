6e2e54fdb7ac5bcd93ea78f39bd41e3b26aaa074
sevsilver1982 <suvorovev@mail.ru>
2020-02-26 23:49:15 +0500

    5. Создать программу 'Консольный чат'. [#222074]

--- chapter_006/src/main/java/inout/Chat.java
+++ chapter_006/src/main/java/inout/Chat.java
@@ -1,9 +1,27 @@
 package inout;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Chat {
-    private Scanner scanner = new Scanner(System.in);
+    private final static String COMMAND_CONTINUE = "продолжить";
+    private final static String COMMAND_STOP = "стоп";
+    private final static String COMMAND_EXIT = "закончить";
+    private final Scanner scanner;
+    private final String fileAnswers;
+
+    public Chat(InputStream inputStream, PrintStream printStream, String fileAnswers) throws FileNotFoundException {
+        this.scanner = new Scanner(inputStream);
+        this.fileAnswers = fileAnswers;
+        talk(printStream);
+    }
+
+    public String getRandomAnswer(String fileAnswers) throws FileNotFoundException {
+        long count = new BufferedReader(new FileReader(fileAnswers)).lines().count();
+        long i = (int) (Math.random() * count);
+        BufferedReader read = new BufferedReader(new FileReader(fileAnswers));
+        return read.lines().skip(i).findFirst().get();
+    }
 
     public String read() {
         if (!scanner.hasNext()) {
@@ -13,29 +31,30 @@ public class Chat {
         return scanner.nextLine();
     }
 
-    public boolean process(String cmd) {
+    public boolean process(String cmd, PrintStream printStream) throws FileNotFoundException {
         switch (cmd.trim().toLowerCase()) {
-            case ("продолжить"):
+            case (COMMAND_CONTINUE):
                 break;
-            case ("стоп"):
+            case (COMMAND_STOP):
                 break;
-            case ("закончить"):
+            case (COMMAND_EXIT):
                 return false;
             default:
+                printStream.println(getRandomAnswer(fileAnswers));
                 break;
         }
         return true;
     }
 
-    public void start() {
+    public void talk(PrintStream printStream) throws FileNotFoundException {
         boolean loop = true;
         while (loop) {
-            loop = process(read());
+            loop = process(read(), printStream);
         }
     }
 
-    public static void main(String[] args) {
-        new Chat().start();
+    public static void main(String[] args) throws IOException {
+        new Chat(System.in, System.out, "c:/soft/answers.txt");
     }
 
 }
