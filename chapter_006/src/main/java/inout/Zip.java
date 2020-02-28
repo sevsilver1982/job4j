472e82b57d34df078e5830311bed4478e99eaec0
sevsilver1982 <suvorovev@mail.ru>
2020-02-28 14:37:30 +0500

    4. Архивировать проект. [#222073]

--- chapter_006/src/main/java/inout/Zip.java
+++ chapter_006/src/main/java/inout/Zip.java
@@ -3,14 +3,21 @@ package inout;
 import java.io.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
     private static List<String> exclude;
-    private static String directory;
-    private static String output;
+    private static String directory = "";
+    private static String output = "";
+
+    static class InvalidParametersException extends Exception {
+        public InvalidParametersException() {
+            super();
+        }
+    }
 
     public void zipDirectory(ZipOutputStream zos, File dir) throws IOException {
         for (File file : dir.listFiles()) {
@@ -38,13 +45,41 @@ public class Zip {
     }
 
     public void pack() throws Exception {
-        File root = new File(directory);
-        if (!root.isDirectory() || !root.exists() || output.isEmpty()) {
-            throw new Exception("Incorrect parameters");
+        if (directory.isEmpty() || output.isEmpty()) {
+            throw new InvalidParametersException();
         }
+        File root = new File(directory);
+        if (!root.isDirectory() || !root.exists()) {
+            throw new InvalidParametersException();
+        }
+
+        Predicate<File> predicate = file ->
+                !file.isDirectory() && file.getPath().endsWith(".txt") || file.getPath().endsWith(".doc");
+
+        List<File> files = new Search().files(directory, predicate);
+
         try (
                 ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(output)))
         ) {
+
+            for (File file : files) {
+                if (file.isDirectory()) {
+
+                    System.out.println(file.getPath());
+                    String entryName = file.getPath()
+                            .replaceAll("\\\\", "/")
+                            .replaceAll(directory, "");
+                    zos.putNextEntry(
+                            new ZipEntry(entryName.startsWith("/") ? entryName.substring(1) : entryName)
+                    );
+                    zos.write(
+                            new BufferedInputStream(new FileInputStream(file)).readAllBytes()
+                    );
+                    zos.closeEntry();
+
+                }
+            }
+
             zipDirectory(zos, root);
         } catch (Exception e) {
             e.printStackTrace();
