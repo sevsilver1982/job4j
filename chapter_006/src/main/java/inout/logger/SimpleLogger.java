adb323993f90692847fbd8efc766f774327b0469
sevsilver1982 <suvorovev@mail.ru>
2020-03-11 21:46:25 +0500

    #R

--- chapter_006/src/main/java/inout/logger/SimpleLogger.java
+++ chapter_006/src/main/java/inout/logger/SimpleLogger.java
@@ -1,20 +1,25 @@
 package inout.logger;
 
-import java.io.PrintStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class SimpleLogger {
-    private PrintStream out;
+    private OutputStream out;
     private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
 
-    public SimpleLogger(PrintStream out) {
+    public SimpleLogger(OutputStream out) {
         this.out = out;
     }
 
     public String log(String msg) {
         String result = String.format("%s : %s",  formatter.format(LocalDateTime.now()), msg);
-        out.println(result);
+        try {
+            out.write(result.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return result;
     }
 
