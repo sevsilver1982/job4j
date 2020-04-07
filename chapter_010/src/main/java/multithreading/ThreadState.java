17c74166f404de9c851de7a51153f6951555c358
sevsilver1982 <suvorovev@mail.ru>
2020-04-08 00:32:25 +0500

    1.1. Состояние нити.[#245463]

---
+++ chapter_010/src/main/java/multithreading/ThreadState.java
@@ -0,0 +1,58 @@
+package multithreading;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        /**
+         * Collect threads
+         */
+        List<Thread> threadList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            threadList.add(
+                    new Thread(
+                            () -> {
+                                try {
+                                    System.out.println(
+                                            String.format("begin %s %s", Thread.currentThread().getName(), Thread.currentThread().getState())
+                                    );
+                                    Thread.sleep(1000);
+                                    System.out.println(
+                                            String.format("end %s %s", Thread.currentThread().getName(), Thread.currentThread().getState())
+                                    );
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            })
+            );
+        }
+
+        /**
+         * Run all threads
+         */
+        for (Thread thread : threadList) {
+            thread.start();
+        }
+
+        /**
+         * Wait until all threads terminated
+         */
+        boolean allThreadTerminated = false;
+        while (!allThreadTerminated) {
+            allThreadTerminated = true;
+            for (Thread thread : threadList) {
+                System.out.println(
+                        String.format("%s %s", thread.getName(), thread.getState())
+                );
+                if (thread.getState() != Thread.State.TERMINATED) {
+                    allThreadTerminated = false;
+                }
+            }
+        }
+
+        System.out.println("all threads terminated");
+    }
+
+}
