838c2a4f6af538accd1e657a98bc68493eb5207a
sevsilver1982 <suvorovev@mail.ru>
2020-04-10 13:40:43 +0500

    3. Прерывание нити[#245466]

---
+++ chapter_010/src/main/java/multithreading/ConsoleProgress.java
@@ -0,0 +1,33 @@
+package multithreading;
+
+public class ConsoleProgress implements Runnable{
+    private final char[] progress = new char[] {
+      '―', '\\', '|', '/'
+    };
+
+    @Override
+    public void run() {
+        int step = 0;
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                Thread.sleep(500);
+                System.out.printf(String.format("\r load: %s", progress[step]));
+                step = step == 3 ? 0 : step + 1;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+
+}
