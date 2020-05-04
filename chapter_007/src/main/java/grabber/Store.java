08bfae8e546dbb7e03ab63dfc3c179eeed26007f
sevsilver1982 <suvorovev@mail.ru>
2020-05-04 18:34:51 +0500

    3. Парсер вакансий на sql.ru[#222015] #R

--- chapter_007/src/main/java/grabber/Store.java
+++ chapter_007/src/main/java/grabber/Store.java
@@ -10,50 +10,7 @@ import liquibase.resource.ClassLoaderResourceAccessor;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-import static grabber.Constants.*;
-
-public class Store implements IStore, AutoCloseable {
-    private static final String CHANGE_LOG_FILE = "db/scripts/grabber/grabber.xml";
-    private Properties properties;
-    private Connection connection;
-
-    public Store(Properties properties) {
-        this.properties = properties;
-    }
-
-    public Connection connect() throws SQLException, ClassNotFoundException {
-        Class.forName(properties.getProperty(PROPERTY_JDBC_DRIVER));
-        return DriverManager.getConnection(
-                properties.getProperty(PROPERTY_JDBC_URL),
-                properties.getProperty(PROPERTY_JDBC_USERNAME),
-                properties.getProperty(PROPERTY_JDBC_PASSWORD)
-        );
-    }
-
-    public boolean init(boolean validateStruct) {
-        try {
-            connection = connect();
-            if (validateStruct) {
-                Liquibase liquibase = new liquibase.Liquibase(
-                        CHANGE_LOG_FILE,
-                        new ClassLoaderResourceAccessor(),
-                        DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connect()))
-                );
-                liquibase.update(new Contexts(), new LabelExpression());
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return connection != null;
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
 
     @Override
     public void close() throws Exception {
