e3b038db9b239602adb1c5671e60f6f1631c43c5
sevsilver1982 <suvorovev@mail.ru>
2020-05-05 23:32:27 +0500

    3. Junit тест для блокирующей очереди.[#245458]

--- chapter_010/src/test/java/multithreading/queue/SimpleBlockingQueueTest.java
+++ chapter_010/src/test/java/multithreading/queue/SimpleBlockingQueueTest.java
@@ -2,6 +2,13 @@ package multithreading.queue;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SimpleBlockingQueueTest {
 
     @Test
@@ -34,6 +41,28 @@ class SimpleBlockingQueueTest {
         producer.start();
         producer.join();
         consumer.join();
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final List<Integer> buffer = new ArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(() -> IntStream.range(0, 5).forEach(queue::offer));
+        producer.start();
+        Thread consumer = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted() || queue.size() > 0) {
+                try {
+                    buffer.add(queue.poll());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertEquals(Arrays.asList(0, 1, 2, 3, 4), buffer);
     }
 
 }
