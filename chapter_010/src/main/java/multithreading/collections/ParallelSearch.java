29c594adb5bea0dacf674669b7afd55b027fdfba
sevsilver1982 <suvorovev@mail.ru>
2020-05-05 23:11:20 +0500

    2. Обеспечить остановку потребителя.[#245457]

--- chapter_010/src/main/java/multithreading/collections/ParallelSearch.java
+++ chapter_010/src/main/java/multithreading/collections/ParallelSearch.java
@@ -1,13 +1,12 @@
-package multithreading.queue;
+package multithreading.collections;
 
-import org.junit.jupiter.api.Test;
+import multithreading.queue.SimpleBlockingQueue;
 
-class SimpleBlockingQueueTest {
+public class ParallelSearch {
 
-    @Test
-    public void test() throws InterruptedException {
+    public static void main(String[] args) {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
-        Thread consumer = new Thread(() -> {
+        final Thread consumer = new Thread(() -> {
             while (!Thread.currentThread().isInterrupted() || queue.size() > 0) {
                 try {
                     System.out.println(String.format("consumer poll element %s", queue.poll()));
@@ -15,25 +14,22 @@ class SimpleBlockingQueueTest {
                     Thread.currentThread().interrupt();
                 }
             }
-            System.out.println("consumer end");
         });
-        Thread producer = new Thread(() -> {
-            for (int i = 0; i != 10; i++) {
+        final Thread producer = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
                 queue.offer(i);
-                System.out.println(i);
+                System.out.println(String.format("producer offer element %s", i));
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(500);
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
             }
-            consumer.interrupt();
             System.out.println("producer end");
+            consumer.interrupt();
         });
         consumer.start();
         producer.start();
-        producer.join();
-        consumer.join();
     }
 
 }
