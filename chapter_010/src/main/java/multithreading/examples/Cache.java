488d0c800311faa37b4f357413295d4ec74eddc7
sevsilver1982 <suvorovev@mail.ru>
2020-05-09 09:43:09 +0500

    0. Что такое атомарность?[#245470] #R как сделать атомарно?

--- chapter_010/src/main/java/multithreading/examples/Cache.java
+++ chapter_010/src/main/java/multithreading/examples/Cache.java
@@ -1,17 +1,17 @@
 package multithreading.examples;
 
-public final class Cache {
-    private static Cache cache;
+import java.util.concurrent.atomic.AtomicReference;
 
-    private synchronized static Cache instOf() {
-        if (cache == null) {
-            cache = new Cache();
-        }
+public final class Cache {
+    private static AtomicReference<Cache> cache = new AtomicReference<>();
+
+    private static AtomicReference<Cache> instOf() {
+        cache.compareAndSet(null, new Cache());
         return cache;
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 50; i++) {
             new Thread(() -> System.out.println(instOf())).start();
         }
     }
